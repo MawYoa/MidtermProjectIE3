95dc4ed2d7827d24c62b4e7794df6428dce934f2
JhonoBrine <jhonpabroa@gmail.com>
2024-10-04 13:35:18 +0800

    Test

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import streamlit as st
+import os
 
-
-st.title('My First Streamlit App')
-st.write('Welcome to my Streamlit app!')
-
+file = "D:\$FILES\SCHOOL_FILES\#Fourth_Year\First_Semester\CSIT342_Industry_Elective_3\MidtermProject\MidtermProjectIE3\main.py"
+thisfile = os.path.abspath(file)
+if ('/' in thisfile): os.chdir(os.path.dirname(thisfile))
 
 # -- PAGE SETUP --
 
 profile_1_page= st.Page(
-    page="MidtermProjectIE3/views/main_page.py",
+    
+    page="MProject/views/mainpage.py",
     title="First Page",
     icon=":material/account_circle:",
     default=True,
+)
+
+profile_2_page= st.Page(
+    
+    page="MProject/views/dataPage.py",
+    title="Data Page",
+    icon=":material/account_circle:",
+)
+
+pg = st.navigation(
+    {
+        "Info": [profile_1_page, profile_2_page]
+    }    
 )
